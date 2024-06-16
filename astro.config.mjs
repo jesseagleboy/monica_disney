6cc21e292d3913f4dfa4b884fa31574996c81091
jesseagleboy <jslomowitz@gmail.com>
2024-06-16 10:12:14 -0400

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
-export default defineConfig( {
+export default defineConfig({
   site: 'http://localhost:3000',
-  integrations: [tailwind(), mdx(), sitemap()]
+  integrations: [tailwind(), mdx(), sitemap()],
+  output: "static",
+  adapter: vercel()
 });
