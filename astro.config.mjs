53418a483caae11f47839e470d6ca0c6bc4d5e7b
jesseagleboy <jslomowitz@gmail.com>
2024-06-21 01:41:46 -0400

    begin about_me page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/static";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://magicaladventuresbymonica.com/',
-  prefetch: true,
-  integrations: [ tailwind( {
-    applyBaseStyles: false,
-  }), mdx(), sitemap()],
-  output: "static",
-  adapter: vercel()
+	site: "https://magicaladventuresbymonica.com/",
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		mdx(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
+	output: "static",
+	adapter: vercel(),
 });
