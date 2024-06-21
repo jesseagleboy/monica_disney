1c757e534462a30ae3ebc903a6cc2c86570e8064
jesseagleboy <jslomowitz@gmail.com>
2024-06-21 03:43:36 -0400

    small variable changes

--- src/functions/doConfetti.ts
+++ src/functions/doConfetti.ts
@@ -1,28 +1,27 @@
 import confetti from "canvas-confetti";
-async function doConfetti ()
-{
-		var duration = 7 * 1000;
-		var animationEnd = Date.now() + duration;
-		var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+async function doConfetti() {
+	const duration = 7 * 1000;
+	const animationEnd = Date.now() + duration;
+	const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-		function randomInRange(min: number, max: number) {
-			return Math.random() * (max - min) + min;
+	function randomInRange(min: number, max: number) {
+		return Math.random() * (max - min) + min;
+	}
+
+	const interval: ReturnType<typeof setInterval> = setInterval(function () {
+		var timeLeft = animationEnd - Date.now();
+
+		if (timeLeft <= 0) {
+			return clearInterval(interval);
 		}
 
-		var interval: ReturnType<typeof setInterval> = setInterval(function () {
-			var timeLeft = animationEnd - Date.now();
+		const particleCount = 50 * (timeLeft / duration);
+		// since particles fall down, start a bit higher than random
+		confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+		confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+	}, 250);
 
-			if (timeLeft <= 0) {
-				return clearInterval(interval);
-			}
-
-			var particleCount = 50 * (timeLeft / duration);
-			// since particles fall down, start a bit higher than random
-			confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-			confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
-		}, 250);
-
-		return () => clearInterval(interval);
+	return () => clearInterval(interval);
 }
-    
-export default doConfetti
+
+export default doConfetti;
