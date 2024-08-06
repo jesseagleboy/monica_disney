5351cf485c12713925497e106dfa3900d6ccefa2
jesseagleboy <jslomowitz@gmail.com>
2024-08-05 20:33:30 -0400

    sanity base setup - url errors to be fixed

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,20 @@
 // ./sanity.config.ts
 import { defineConfig } from "sanity";
-import {visionTool} from '@sanity/vision'
+import { visionTool } from "@sanity/vision";
 import { structureTool } from "sanity/structure";
-import { schemaTypes } from "@/sanitymonicadisneysite/schemaTypes"
+import { schemaTypes } from "@/sanitymonicadisneysite/schemaTypes";
+import { presentationTool } from "sanity/presentation";
 
 export default defineConfig({
 	projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
 	dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-	plugins: [structureTool(), visionTool()],
+	plugins: [
+		structureTool(),
+		visionTool(),
+		presentationTool({
+			previewUrl: location.origin,
+		}),
+	],
 
 	schema: {
 		types: schemaTypes,
