45b575a02889915b54f7ab28939e207c16fc7ea7
jesseagleboy <jslomowitz@gmail.com>
2024-08-06 11:57:15 -0400

    sanity editor test for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,38 +2,33 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://magicaladventuresbymonica.com/",
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		mdx(),
-		sitemap(),
-		partytown({
-			config: {
-				forward: ["dataLayer.push"],
-				debug: true,
-			},
-		}),
-		sanity({
-			projectId: "gwwfkmev",
-			dataset: "production",
-			apiVersion: "2024-08-05",
-			useCdn: false,
-			studioBasePath: "/studio",
-			stega: {
-				studioUrl: "/studio",
-			},
-		}),
-		react(),
-	],
-	output: "server",
-	adapter: vercel(),
+  site: "https://magicaladventuresbymonica.com/",
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+      debug: true
+    }
+  }), sanity({
+    projectId: "gwwfkmev",
+    dataset: "production",
+    apiVersion: "2024-08-05",
+    useCdn: false,
+    studioBasePath: "/studio",
+    stega: {
+      studioUrl: "/studio"
+    }
+  }), react()],
+  output: "hybrid",
+  adapter: netlify()
 });
