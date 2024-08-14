161ea94442aa673ce213a5b5df6fb5c270cfe01f
jesseagleboy <jslomowitz@gmail.com>
2024-08-14 14:03:37 -0400

    pwa setup with icon templates

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,14 +52,26 @@ export default defineConfig({
 				theme_color: "#ffffff",
 				icons: [
 					{
-						src: "favicon.svg",
+						src: "pwa-64x64.png",
+						sizes: "64x64",
+						type: "image/png",
+					},
+					{
+						src: "pwa-192x192.png",
 						sizes: "192x192",
 						type: "image/png",
 					},
 					{
-						src: "favicon.svg",
+						src: "pwa-512x512.png",
 						sizes: "512x512",
 						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "maskable-icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
 					},
 				],
 			},
