be6468b201aa1e85d13248ed50bf4fce2a36c434
jesseagleboy <jslomowitz@gmail.com>
2024-08-20 19:54:54 -0400

    added pwa assets to layout

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,4 @@
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
+/// <reference types="vite-plugin-pwa/pwa-assets" />
