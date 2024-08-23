64502c37cc1efe3c9bf9a86a530c300f0b17bfea
jesseagleboy <jslomowitz@gmail.com>
2024-08-23 01:19:46 -0400

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,64 +12,79 @@ import tunnel from "astro-tunnel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.magicaladventuresbymonica.com/",
-  server: {
-    open: true
-  },
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), sanity({
-    projectId: "gwwfkmev",
-    dataset: "production",
-    apiVersion: "2024-08-20",
-    useCdn: false,
-    studioBasePath: "/studio",
-    stega: {
-      studioUrl: "/studio"
-    }
-  }), react(), AstroPWA({
-    includeAssets: ["favicon.svg"],
-    registerType: "autoUpdate",
-    devOptions: {
-      enabled: true
-    },
-    workbox: {
-      navigateFallback: "/404",
-      globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"]
-    },
-    manifest: {
-      name: "Magical Adventures by Monica",
-      short_name: "Magical Adventures by Monica",
-      description: "Monica DeFosse - Once Upon A Wish Travel",
-      theme_color: "#ffffff",
-      icons: [{
-        src: "pwa-64x64.png",
-        sizes: "64x64",
-        type: "image/png"
-      }, {
-        src: "pwa-192x192.png",
-        sizes: "192x192",
-        type: "image/png"
-      }, {
-        src: "pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any"
-      }, {
-        src: "maskable-icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable"
-      }]
-    },
-    pwaAssets: {
-      config: true
-    }
-  } ), tunnel()],
-  output: "hybrid",
-  adapter: netlify()
+	site: "https://www.magicaladventuresbymonica.com/",
+	server: {
+		open: true,
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		mdx(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+				debug: true,
+			},
+		}),
+		sanity({
+			projectId: "gwwfkmev",
+			dataset: "production",
+			apiVersion: "2024-08-20",
+			useCdn: false,
+			studioBasePath: "/studio",
+			stega: {
+				studioUrl: "/studio",
+			},
+		}),
+		react(),
+		AstroPWA({
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			workbox: {
+				navigateFallback: "/404",
+				globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+			},
+			manifest: {
+				name: "Magical Adventures by Monica",
+				short_name: "Magical Adventures by Monica",
+				description: "Monica DeFosse - Once Upon A Wish Travel",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-64x64.png",
+						sizes: "64x64",
+						type: "image/png",
+					},
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "maskable-icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+			},
+			pwaAssets: {
+				config: true,
+			},
+		}),
+		tunnel(),
+	],
+	output: "hybrid",
+	adapter: netlify(),
 });
